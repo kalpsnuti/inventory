d23083c080af56441fce585c05023c5a298d196f
Gaurav J <gaurav.joshi@programmer.net>
2020-09-25 14:04:17 +0530

    Updates

---
+++ precache-manifest.998acbde9ecd1ba35e70d38230359f8b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc8190e2abc3372e632cd489f9093db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0132ee3f971af516ec",
+    "url": "/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "1c73a1d281b01e85f0fe",
+    "url": "/static/css/main.5b844fad.chunk.css"
+  },
+  {
+    "revision": "bc0132ee3f971af516ec",
+    "url": "/static/js/2.9ec09985.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.9ec09985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c73a1d281b01e85f0fe",
+    "url": "/static/js/main.a414cca4.chunk.js"
+  },
+  {
+    "revision": "f6454dff75e3206d905f",
+    "url": "/static/js/runtime-main.e0dcac5e.js"
+  },
+  {
+    "revision": "8a305f7c6165b795fdbdcbcaddda8dd5",
+    "url": "/static/media/back.8a305f7c.jpg"
+  },
+  {
+    "revision": "1fef3a090cc59dd8f776a58152826143",
+    "url": "/static/media/button.1fef3a09.jpg"
+  },
+  {
+    "revision": "ee3a0100a7562105757497083f220711",
+    "url": "/static/media/front.ee3a0100.jpg"
+  },
+  {
+    "revision": "e03ec62c3d0a6d79063c578b2d7b8f57",
+    "url": "/static/media/frontBack.e03ec62c.jpg"
+  },
+  {
+    "revision": "c7767a753ee7774ba542c52d5e0bd102",
+    "url": "/static/media/slider.c7767a75.jpg"
+  }
+]);
